8304daf7ca509777826668467a321ddcc34206c6
Katya Railian <katerina.raylyan@gmail.com>
2020-11-15 23:55:52 +0300

    fix: loads right styles sheet

--- webpack.dev.js
+++ webpack.dev.js
@@ -26,21 +26,18 @@ module.exports = {
                 test: /\.(scss|css)$/,
                 use: [
                     {
-                        // creates style nodes from JS strings
                         loader: "style-loader",
                         options: {
                             sourceMap: true
                         }
                     },
                     {
-                        // translates CSS into CommonJS
                         loader: "css-loader",
                         options: {
                             sourceMap: true
                         }
                     },
                     {
-                        // compiles Sass to CSS
                         loader: "sass-loader",
                         options: {
                             outputStyle: 'expanded',
@@ -48,18 +45,15 @@ module.exports = {
                             sourceMapContents: true
                         }
                     }
-                    // Please note we are not running postcss here
                 ]
             }
             ,
             {
-                // Load all images as base64 encoding if they are smaller than 8192 bytes
                 test: /\.(png|jpg|gif)$/,
                 use: [
                     {
                         loader: 'url-loader',
                         options: {
-                            // On development we want to see where the file is coming from, hence we preserve the [path]
                             name: '[path][name].[ext]?hash=[hash:20]',
                             limit: 8192
                         }
@@ -68,7 +62,6 @@ module.exports = {
             }
             ,
             {
-                // Load all icons
                 test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
                 use: [
                     {
